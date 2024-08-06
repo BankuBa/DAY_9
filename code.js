0d2de3c0ae8ab94f5631668fce281b7c129959cb
BankuBa <paaekow04@gmail.com>
2024-08-06 21:58:41 +0000

    first commit

---
+++ code.js
@@ -0,0 +1,46 @@
+let question = document.querySelector(".question");
+question.innerHTML = "slim";
+
+let questions = [
+  {
+    question: "What is the capital of Ghana?",
+    answer: [
+      { text: "Accra", correct: true },
+      { text: "Kumasi", correct: false },
+      { text: "Tamale", correct: false },
+      { text: "Takoradi", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    answer: [
+      { text: "Earth", correct: false },
+      { text: "Jupiter", correct: true },
+      { text: "Mars", correct: false },
+      { text: "Venus", correct: false },
+    ],
+  },
+  {
+    question: 'Who wrote "To Kill a Mockingbird"?',
+    answer: [
+      { text: "Harper Lee", correct: true },
+      { text: "Mark Twain", correct: false },
+      { text: "Ernest Hemingway", correct: false },
+      { text: "F. Scott Fitzgerald", correct: false },
+    ],
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    answer: [
+      { text: "Au", correct: true },
+      { text: "Ag", correct: false },
+      { text: "Pb", correct: false },
+      { text: "Fe", correct: false },
+    ],
+  },
+];
+
+
+function show_questions() {
+  let 
+}
